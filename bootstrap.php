4bfbb5aab05bc9cef31c598c836ab74d1f669665
Alessio Pulzoni <alessio.pulzoni@studio.unibo.it>
2026-01-18 19:47:51 +0100

    commit di prova2

--- bootstrap.php
+++ bootstrap.php
@@ -4,5 +4,3 @@ require_once(__DIR__ . "/utils/functions.php");
 require_once(__DIR__ . "/database/DatabaseHelper.php");
 $dbh = new DatabaseHelper("localhost", "root", "", "campus_sports_arena", 3306);
 ?>
-
-//ciao 
